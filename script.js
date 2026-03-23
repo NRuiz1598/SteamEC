e8e102d2dd023b9b965f7882884e79352e1fcd1d
NRuiz1598 <nruiz1512@gmail.com>
2026-03-23 18:56:15 -0500

    ✨ Feat: agrego funcion calcular basica sin logica de IVA

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const precio = document.getElementById('precio');
 const boton = document.getElementById('boton');
 const total = document.getElementById('total');
 
+function calcular () {
+    total.innerHTML = `Total: $${precio.value}`;
+    precio.value = "";
+}
