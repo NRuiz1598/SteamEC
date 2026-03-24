bf40f505bc75eaf02454afb70e2a073033942e4c
NRuiz1598 <nruiz1512@gmail.com>
2026-03-24 17:00:29 -0500

    ✨ Feat: Convierto input a float y agrego IVA actual de Ecuador

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const boton = document.getElementById('boton');
 const total = document.getElementById('total');
 
 function calcular () {
+    const precioFloat= parseFloat(precio.value);
+    const iva = 0.15;
     total.innerHTML = `Total: $${precio.value}`;
     precio.value = "";
 }
