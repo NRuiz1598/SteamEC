d76cdced1701f9670efed5ed206d1941765af896
NRuiz1598 <nruiz1512@gmail.com>
2026-03-25 16:20:55 -0500

    ✨ Feat: agrego variables para el ISD y tarifa bancaria y defino su valor

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const price = document.getElementById('price');
 const button = document.getElementById('button');
 const total = document.getElementById('total');
+const isdTax = document.getElementById('isdTax');
+const bankFee = document.getElementById('bankFee');
 
 function calc () {
-    const floatPrice = parseFloat(price.value);
     const iva = 0.15;
+    const isdTax = 0.05;
+    const bankFee = 0.02;
+
+    
+    const floatPrice = parseFloat(price.value);
     const totalIva = floatPrice + (floatPrice * iva); 
     total.innerHTML = `Total: $${totalIva.toFixed(2)}`;
     price.value = "";
+
+
 }
 
 boton.addEventListener('click', calc);
