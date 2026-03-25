9ec27d5698e6a18cf286c88ec27a04bfdd8dde7c
NRuiz1598 <nruiz1512@gmail.com>
2026-03-25 17:51:34 -0500

    ✨ Feat: agrego variables con formulas necesarias ✨ Refactor: paso valores al scope global para posible reuso

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const price = document.getElementById('price');
 const button = document.getElementById('button');
 const total = document.getElementById('total');
-const isdTax = document.getElementById('isdTax');
+const ISDTax = document.getElementById('isdTax');
 const bankFee = document.getElementById('bankFee');
+const IVA = 0.15;
+const ISDValue = 0.05;
+const bankFeeValue = 0.02;
+
 
 function calc () {
-    const iva = 0.15;
-    const isdValue = 0.05;
-    const bankFeeValue = 0.02;
-
 
     const floatPrice = parseFloat(price.value);
-    const totalIva = floatPrice + (floatPrice * iva); 
-    total.innerHTML = `Total: $${totalIva.toFixed(2)}`;
+    const totalIVA = floatPrice *   IVA;
+    const totalISD = floatPrice * ISDValue;
+    const totalPrice =  floatPrice + totalIVA;
+    const totalTogglesOn = floatPrice + (totalIVA + totalISD);
+    const totalBank = totalPrice * bankFeeValue;
+
+
+    total.innerHTML = `Total: $${totalPrice.toFixed(2)}`;
     price.value = "";
+
+    if (ISDTax.checked) {
+        totalIVA
+    }
 
 
 }
